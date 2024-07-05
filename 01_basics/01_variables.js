7f7e993016ace1ea7e5e1aea68f77d77031b4f7e
Ganesh Subhash Pandit <panditganesh646@gmail.com>
2024-07-05 17:08:50 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "ganesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//  accountId = 2 //not Allowed
+
+accountEmail = "ganesh646@gmail.com"
+accountPassword = "212213"
+accountCity = "bengaluru"
+let accountState;
+
+console.log(accountId);
+/*
+Prefer not to use var 
+because of issue in block scope and functional scop
+*/
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState]);
+ 
